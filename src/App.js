183b8dd2366191c8bc853d68938c5e8a303405f8
phenk53 <phenk@hs-mittweida.de>
2020-06-24 17:12:50 +0200

    first commit to the final version of this project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+
+import Greeter from './artifacts/Greeter.json';
+import drizzleOptions from './drizzleOptions';
+import { DrizzleProvider } from '@drizzle/react-plugin';
+
+// Tells Drizzle what we need to run the smart contract
+//Create a drizzleOptions object and pass in the desired contract artifacts for Drizzle to instantiate.
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <DrizzleProvider options={drizzleOptions}>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div className="container">
+      <a className="navbar-brand" href="#">Block, (Toilet-)Paper, Scissors</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarResponsive">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Spiel
+              <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Anleitung</a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link" href="#">Kontakt</a>
+          </li>
+        </ul>
+      </div>
     </div>
+  </nav>
+    </DrizzleProvider>
   );
 }
 
---
+++ src/drizzleOptions.js
@@ -0,0 +1,7 @@
+import Greeter from './artifacts/Greeter.json';
+
+const drizzleOptions = {
+    contract: [Greeter]
+}
+
+export default drizzleOptions;
